59aace9c2393781cc641f0a86dd6f53aefdec6bb
LeoOlivaresD <cspraword@gmail.com>
2023-03-29 22:37:23 -0300

    Refactorizacion

--- src/main/java/com/api/crud/repositories/IUserRepository.java
+++ src/main/java/com/api/crud/repositories/IUserRepository.java
@@ -1,5 +1,4 @@
 package com.api.crud.repositories;
-
 import com.api.crud.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
