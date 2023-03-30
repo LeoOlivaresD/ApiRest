9ecdd6979a020bd91d79b2a2e1fc4adbd8934e69
LeoOlivaresD <cspraword@gmail.com>
2023-03-29 22:39:08 -0300

    Se agregan rutas para cada peticion y ademas se agrega la notacion @PathVariable en nuestro metodo update, de esta forma logramos hacer correctamente la peticion por postman

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -1,21 +1,17 @@
 package com.api.crud.controllers;
-
 import com.api.crud.models.UserModel;
 import com.api.crud.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/get")
+    @GetMapping("/readAll")
     public ArrayList<UserModel> getUsers(){
         return this.userService.getUsers();
     }
@@ -24,19 +20,19 @@ public class UserController {
     public UserModel user(@RequestBody UserModel user){
         return userService.createUser(user);
     }
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/readOne/{id}")
     public Optional<UserModel> getOneById(@PathVariable Long id){
         return userService.getOne(id);
     }
-    @PutMapping(path = "/{id}")
-    public UserModel updateUserById(@RequestBody UserModel request , Long id){
+    @PutMapping(path = "{id}")
+    public UserModel updateUserById(@RequestBody UserModel request ,@PathVariable Long id){
     return this.userService.updateByID(request, id);
     }
-    @DeleteMapping (path = "/{id}")
+    @DeleteMapping (path = "/delete/{id}")
     public String deleteById(@PathVariable ("id") Long id){
         boolean ok = this.userService.deleteUser(id);
         if (ok){
-            return "User with id " + id + "deleted!";
+            return "User with id " + id + " deleted!";
         }else {
             return "Error, we have a problem and can´t delete user with id " + id ;
         }
